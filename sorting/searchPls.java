410b1fb3cdcfd45dee92ceeef4f93cee4a2e712e
Vipin <64849587+vipinpal70@users.noreply.github.com>
2023-10-16 21:46:47 +0530

    Allocation of Book page Done

--- sorting/searchPls.java
+++ sorting/searchPls.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 
 /*
- * all question related to searching and sorting 
+ * all Important question related to searching and sorting 
  */
 public class searchPls {
 
@@ -27,7 +27,9 @@ public class searchPls {
         return true;
     }
 
-    // Find common elements in three sorted arrays
+    /*
+     * Find common elements in three sorted arrays
+     */
     public static void commonElements(int[] a1, int[] a2, int[] a3) {
         int max = a1[0];
         int finalMax = a1[a1.length - 1];
@@ -68,7 +70,9 @@ public class searchPls {
 
     }
 
-    // Count triplets with sum smaller than a given value
+    /*
+     * Count triplets with sum smaller than a given value
+     */
 
     public static int countTriplets(int[] arr, int sum) {
         // Sort input array
@@ -104,8 +108,10 @@ public class searchPls {
         return ans;
     }
 
-    // Maximum sum such that no two elements are adjacent (Stickler Thief)
-    // Function to find the maximum sum
+    /*
+     * Maximum sum such that no two elements are adjacent (Stickler Thief)
+     * Function to find the maximum sum
+     */
     static int rec(int nums[], int idx, int N) {
         if (idx >= N)
             return 0;
@@ -118,7 +124,9 @@ public class searchPls {
         return rec(arr, 0, N);
     }
 
-    // Merge two sorted arrays with O(1) extra space
+    /*
+     * Merge two sorted arrays with O(1) extra space
+     */
     public static void merge(int[] arr1, int[] arr2) {
         int m = arr1.length;
         int n = arr2.length;
@@ -145,7 +153,9 @@ public class searchPls {
         }
     }
 
-    // Count Inversions
+    /*
+     * Count Inversions
+     */
     public static int countInversion(int[] arr) {
         int ans = 0;
         int N = arr.length;
@@ -162,7 +172,9 @@ public class searchPls {
         return ans;
     }
 
-    // merge sort Approach
+    /*
+     * merge sort Approach
+     */
     public static long countInversionUsingMerge(int[] arr) {
         int N = arr.length;
 
@@ -211,7 +223,9 @@ public class searchPls {
         return count;
     }
 
-    // find duplicate in an array in O(n) ans constant space
+    /*
+     * find duplicate in an array in O(n) ans constant space
+     */
     public static List<Integer> findDuplicates(int[] arr) {
         List<Integer> duplicates = new ArrayList<>();
         int n = arr.length;
@@ -240,7 +254,9 @@ public class searchPls {
         return duplicates;
     }
 
-    // product of array except itself
+    /*
+     * product of array except itself
+     */
     public static void productItself(int[] arr) {
         // calculate sum
         int total = 1;
@@ -261,7 +277,10 @@ public class searchPls {
         System.out.println();
     }
 
-    // Make all array elements equal with minimum cost
+    /*
+     * Make all array elements equal with minimum cost
+     */
+
     public static int computeCost(int arr[], int N, int X) {
         int cost = 0;
         for (int i = 0; i < N; i++)
@@ -317,7 +336,9 @@ public class searchPls {
         return computeCost(arr, N, (low + high) / 2);
     }
 
-    // Check if reversing a sub array make the array sorted
+    /*
+     * Check if reversing a sub array make the array sorted
+     */
     public static void reverse(int[] a, int x, int y) {
         while (x < y) {
             int temp = a[x];
@@ -355,7 +376,9 @@ public class searchPls {
         return true;
     }
 
-    // findFour related to the question
+    /*
+     * findFour related to the question
+     */
     public static boolean findFour(int[] arr, int i, int j, int k, int end, int value) {
 
         while (arr[i] + arr[j] + arr[k] + arr[end] != value && j < arr.length - 2) {
@@ -382,7 +405,9 @@ public class searchPls {
         return false;
     }
 
-    // Find four elements that sum to a given value (4Sum) | Set 2
+    /*
+     * Find four elements that sum to a given value (4Sum) | Set 2
+     */
     public static boolean fourSum(int[] arr, int value) {
         int N = arr.length;
         Arrays.sort(arr);
@@ -422,8 +447,10 @@ public class searchPls {
         return false;
     }
 
-    // Median of two Sorted Arrays of Different SizesMedian of two Sorted Arrays of
-    // Different Sizes
+    /*
+     * Median of two Sorted Arrays of Different SizesMedian of two Sorted Arrays of
+     * Different Sizes
+     */
     public static void medianOfTwoSortedArr(int[] a, int[] b) {
         int alen = a.length;
         int blen = b.length;
@@ -449,7 +476,10 @@ public class searchPls {
 
     }
 
-    // Best Approach (binary search with divide approach)
+    /*
+     * Best Approach (binary search with divide approach)
+     */
+
     public static double medianOfSortedArray(int[] a, int[] b) {
         int n1 = a.length;
         int n2 = b.length;
@@ -461,7 +491,7 @@ public class searchPls {
         System.out.println("left :" + left);
         int n = n1 + n2;
         while (low <= high) {
-            System.out.println("low : "+low);
+            System.out.println("low : " + low);
             int mid1 = (low + high) / 2;
             System.out.println("mid1 : " + mid1);
             int mid2 = left - mid1;
@@ -493,18 +523,136 @@ public class searchPls {
         return 0;
     }
 
+    /*
+     * Agressive cows (Linear Search)
+     */
+    public static int agressiveCows(int[] arr, int cows) {
+        Arrays.sort(arr);
+        int ans = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (canWePlace(arr, i, cows) == true) {
+                ans = i;
+            } else {
+                return i - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    /*
+     *   Agressive cows (Binary Approach)  Best Approach
+     */
+
+    public static int agressiveCowsBinaryApp(int[] arr , int cows){
+        Arrays.sort(arr);
+        int low = 1, high = arr[arr.length-1] - arr[0];
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(canWePlace(arr, mid, cows) == true){
+                low = mid+1;
+            } else{
+                high = mid-1;
+            }
+        }
+        return high;
+
+    }
+
+    public static boolean canWePlace(int[] arr, int disnt, int cows) {
+        int cowCount = 1;
+        int lastCows = arr[0];
+        for (int j = 1; j < arr.length; j++) {
+            if (arr[j] - lastCows >= disnt) {
+                cowCount++;
+                lastCows = arr[j];
+            }
+            if (cowCount >= cows)
+                return true;
+        }
+        return false;
+    }
+
+
+    /*
+     * Allocate minimum number of pages
+     * ----> Function to find minimum number of pages.
+     */
+    
+    public static int findPages(int[] A, int N, int M) {
+
+        if (N < M) {
+            return -1;
+        }
+        int low = 0;  //max lement in the array
+        int high = 0; // sum of array
+
+        for (int i = 0; i < A.length; i++) {
+
+            if (A[i] > low) {
+                low = A[i];
+            }
+
+            high += A[i];
+        }
+
+        int res = -1;
+
+        while (low <= high) {
+
+            int mid = (low + high) / 2;  // it refers to pages
+
+            if (isAllocatable(A, mid, M)) {
+                res = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+
+        }
+
+        return res;
+    }
+
+    public static boolean isAllocatable(int[] A, int mid, int M) {
+
+        int student = 1;
+        int sum = 0;
+
+        for (int i = 0; i < A.length; i++) {
+
+            if (sum + A[i] > mid) {
+                student++;
+                sum = A[i];
+            } else {
+                sum += A[i];
+            }
+
+        }
+
+        return student <= M;
+
+    }
+
+
     public static void main(String[] args) {
 
         // int[] arr = { 1, 100, 101 };
         // int ans = minCostToMakeElementEqual(arr, arr.length);
         // System.out.println("ans" + ans);
-
         // int[] arr = { 10, 2, 5, 4, 3, 7, 8, 9 };
         // System.out.println(fourSum(arr, 20));
 
-        int[] a = { -5, 3, 6, 12, 15 };
-        int[] b = { -12, -10, -6, -3, 4, 10 };
+        // int[] a = { -5, 3, 6, 12, 15 };
+        // int[] b = { -12, -10, -6, -3, 4, 10 };
         // medianOfTwoSortedArr(a, b);
-        System.out.println(medianOfSortedArray(a, b));
+        // System.out.println(medianOfSortedArray(a, b));
+        // int[] arr = {12,34,67,90};
+        // int re = allocateMinPages(4, arr, 2);
+        // System.out.println(re);
+
+        int[] arr = { 0, 3, 4, 9, 7, 10 };
+        int re = agressiveCowsBinaryApp(arr, 4);
+        System.out.println(re);
     }
 }
